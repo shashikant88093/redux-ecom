c078194610679ab38122555dbcf5423e5b7539d6
shashikant88093 <shashi94goswami@gmail.com>
2020-06-09 11:53:29 +0530

    Updates

---
+++ precache-manifest.35b3f89b3e2171e2784b5a744b8e9e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9986ba43d9dd59c7a982d6c73ec9dcd4",
+    "url": "/redux-ecom/index.html"
+  },
+  {
+    "revision": "387f025ac6c6f046886e",
+    "url": "/redux-ecom/static/css/main.eab7380b.chunk.css"
+  },
+  {
+    "revision": "809b45f7c00e87d7b2dd",
+    "url": "/redux-ecom/static/js/2.ac34d548.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/redux-ecom/static/js/2.ac34d548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387f025ac6c6f046886e",
+    "url": "/redux-ecom/static/js/main.18bec974.chunk.js"
+  },
+  {
+    "revision": "87af7f412a9cbe393d01",
+    "url": "/redux-ecom/static/js/runtime-main.a46c0f48.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/redux-ecom/static/media/ShoppingIcon.30f07071.svg"
+  }
+]);
